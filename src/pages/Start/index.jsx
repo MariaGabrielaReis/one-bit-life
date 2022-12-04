b08a175ecf8010746c2918a788096d0cce095c9d
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2022-12-04 10:12:10 -0300

    feat: create start screen

--- src/pages/Start/index.jsx
+++ src/pages/Start/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { ScrollView, View } from 'react-native';
+import { ScrollView, View, Image, Text, StyleSheet } from 'react-native';
 
 export default function Start() {
   return (
-    <View>
+    <View style={styles.container}>
       <ScrollView showsVerticalScrollIndicator={false}>
-        <View>
-          <Image source={require('../../assets/icons/logo3.png')} />
-          <Text>
+        <View style={{ alignItems: 'center' }}>
+          <Image
+            source={require('../../assets/icons/logo3.png')}
+            style={styles.logo}
+          />
+          <Text style={styles.description}>
             Vamos transformar sua vida {'\n'} em jogo, buscando sempre {'\n'} o
             melhor nível
           </Text>
@@ -16,3 +19,21 @@ export default function Start() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: ' rgba(21, 21, 21, 0.98)',
+  },
+  logo: {
+    width: 300,
+    height: 60,
+    marginVertical: 60,
+  },
+  description: {
+    color: 'white',
+    fontSize: 20,
+    textAlign: 'center',
+    marginVertical: 60,
+  },
+});
